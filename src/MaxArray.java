4cf298da0599f4496152289bfc3d76f272413244
ashishpjr96 <ashishpjr92@gmail.com>
2022-05-03 15:00:41 +0530

    adding maximum array program

---
+++ src/MaxArray.java
@@ -0,0 +1,28 @@
+
+public class MaxArray {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+	  int arr[] = { 5,989,454,345,342,656,543};
+		 
+		  int max =arr[0];
+		  
+		  for (int i=0; i<arr.length;i++)
+
+	{
+	     if(arr[i]>max)
+	    	 
+	    	 max=arr[i];
+	
+	      
+	}
+		  System.out.println("maximum number in arrray is  "+ max);
+	}
+}
+	
+		
+		
